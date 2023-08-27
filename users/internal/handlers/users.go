1bfa59aa03c0b96288c296c1da7cf64b413935bf
MullerJulia <mrjuliamelnik@gmail.com>
2023-08-27 15:20:16 +0300

    Add delete handler and test it

--- users/internal/handlers/users.go
+++ users/internal/handlers/users.go
@@ -21,6 +21,7 @@ type CreateUserParams struct {
 type UsersService interface {
 	Create(ctx context.Context, name string) (models.User, error)
 	GetOne(ctx context.Context, id string) (models.User, error)
+	DeleteOne(ctx context.Context, id string) error
 }
 
 type Users struct {
@@ -36,6 +37,7 @@ func (u Users) Routes() http.Handler {
 
 	r.Post("/", u.Create)
 	r.Get("/{id}", u.GetOne)
+	r.Delete("/{id}", u.DeleteOne)
 
 	return r
 }
@@ -86,4 +88,22 @@ func (u Users) GetOne(w http.ResponseWriter, r *http.Request) {
 	if err := response.JSON(w, usr); err != nil {
 		log.Error().Err(err).Msg("failed to encode response")
 	}
+}
+
+func (u Users) DeleteOne(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	log := logger.FromContext(ctx)
+	id := chi.URLParam(r, "id")
+
+	if err := u.user.DeleteOne(ctx, id); err != nil {
+		if errors.Is(err, models.NotFoundErr) {
+			response.NotFound(w)
+			return
+		}
+		log.Error().Err(err).Msg("failed to delete user")
+		response.InternalError(w)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
--- users/internal/handlers/users_test.go
+++ users/internal/handlers/users_test.go
@@ -172,3 +172,79 @@ func TestUsers_GetOne(t *testing.T) {
 		})
 	}
 }
+
+func TestUsers_DeleteOne(t *testing.T) {
+	type fields struct {
+		user UsersService
+	}
+	type args struct {
+		w *httptest.ResponseRecorder
+		r *http.Request
+	}
+	tests := []struct {
+		name     string
+		fields   fields
+		args     args
+		wantCode int
+		wantBody []byte
+	}{
+		{
+			name: "success",
+			fields: fields{
+				user: &UsersServiceMock{
+					DeleteOneFunc: func(ctx context.Context, id string) error {
+						assert.Equal(t, "1", id)
+						return nil
+					},
+				},
+			},
+			args: args{
+				w: httptest.NewRecorder(),
+				r: httptest.NewRequest(http.MethodDelete, "/1", nil),
+			},
+			wantCode: http.StatusOK,
+			wantBody: nil,
+		},
+		{
+			name: "Internal Server Error",
+			fields: fields{
+				user: &UsersServiceMock{
+					DeleteOneFunc: func(ctx context.Context, id string) error {
+						return errors.New("error")
+					},
+				},
+			},
+			args: args{
+				w: httptest.NewRecorder(),
+				r: httptest.NewRequest(http.MethodDelete, "/1", nil),
+			},
+			wantCode: http.StatusInternalServerError,
+			wantBody: []byte(`Internal Server Error` + "\n"),
+		},
+		{
+			name: "Not Found",
+			fields: fields{
+				user: &UsersServiceMock{
+					DeleteOneFunc: func(ctx context.Context, id string) error {
+						return models.NotFoundErr
+					},
+				},
+			},
+			args: args{
+				w: httptest.NewRecorder(),
+				r: httptest.NewRequest(http.MethodDelete, "/1", nil),
+			},
+			wantCode: http.StatusNotFound,
+			wantBody: []byte(`Not Found` + "\n"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			u := NewUsers(tt.fields.user)
+			u.Routes().ServeHTTP(tt.args.w, tt.args.r)
+
+			assert.Equal(t, tt.wantCode, tt.args.w.Code)
+			assert.Equal(t, string(tt.wantBody), tt.args.w.Body.String(), "unxpected body")
+		})
+	}
+}
